0606e13e1827d4bf468fb25a249651320f676a5b
bpelikan <bartoszpelikan@gmail.com>
2019-02-27 01:36:06 +0100

    Disable registration

---
+++ CarnivorousPlants/CarnivorousPlants/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CarnivorousPlants.Data;
+using CarnivorousPlants.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CarnivorousPlants.Areas.Identity.IdentityHostingStartup))]
+namespace CarnivorousPlants.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
