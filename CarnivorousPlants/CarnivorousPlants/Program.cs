b4450740f09fd94f6de3163e527d6aa273b2c015
bpelikan <bartoszpelikan@gmail.com>
2019-03-28 23:09:14 +0100

    Add Application Insights

--- CarnivorousPlants/CarnivorousPlants/Program.cs
+++ CarnivorousPlants/CarnivorousPlants/Program.cs
@@ -34,6 +34,7 @@ namespace CarnivorousPlants
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
 
     }
