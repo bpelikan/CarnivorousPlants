7509fa65879bce4607fe13a38961e004dc62b45e
bpelikan <bartoszpelikan@gmail.com>
2019-03-30 19:31:52 +0100

    Enable registration

--- CarnivorousPlants/CarnivorousPlants/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ CarnivorousPlants/CarnivorousPlants/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -64,33 +64,37 @@ namespace CarnivorousPlants.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            //returnUrl = returnUrl ?? Url.Content("~/");
-            //if (ModelState.IsValid)
-            //{
-            //    var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
-            //    var result = await _userManager.CreateAsync(user, Input.Password);
-            //    if (result.Succeeded)
-            //    {
-            //        _logger.LogInformation("User created a new account with password.");
+            returnUrl = returnUrl ?? Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser {
+                    UserName = Input.Email,
+                    Email = Input.Email,
+                    CreatedAt = DateTime.UtcNow
+                };
+                var result = await _userManager.CreateAsync(user, Input.Password);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
 
-            //        var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-            //        var callbackUrl = Url.Page(
-            //            "/Account/ConfirmEmail",
-            //            pageHandler: null,
-            //            values: new { userId = user.Id, code = code },
-            //            protocol: Request.Scheme);
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { userId = user.Id, code = code },
+                        protocol: Request.Scheme);
 
-            //        await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-            //            $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-            //        await _signInManager.SignInAsync(user, isPersistent: false);
-            //        return LocalRedirect(returnUrl);
-            //    }
-            //    foreach (var error in result.Errors)
-            //    {
-            //        ModelState.AddModelError(string.Empty, error.Description);
-            //    }
-            //}
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect(returnUrl);
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
 
             //// If we got this far, something failed, redisplay form
             return Page();
