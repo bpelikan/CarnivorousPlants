e4fc9a7482a836c8b8347fe580f608fc1904a85e
bpelikan <bartoszpelikan@gmail.com>
2019-05-20 22:19:04 +0200

    add ImageWaitingToConfirm model

--- CarnivorousPlants/CarnivorousPlants/Data/ApplicationDbContext.cs
+++ CarnivorousPlants/CarnivorousPlants/Data/ApplicationDbContext.cs
@@ -17,6 +17,7 @@ namespace CarnivorousPlants.Data
         public DbSet<MyProject> MyProjects { get; set; }
         public DbSet<MyTag> MyTags { get; set; }
         public DbSet<DefaultProjectHistory> DefaultProjectHistories { get; set; }
+        public DbSet<ImageWaitingToConfirm> ImagesWaitingToConfirm { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
---
+++ CarnivorousPlants/CarnivorousPlants/Models/ImageWaitingToConfirm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarnivorousPlants.Models
+{
+    public class ImageWaitingToConfirm
+    {
+        public Guid ImageWaitingToConfirmId { get; set; }
+
+        //public Guid MyProjectId { get; set; }
+        public Guid MyTagId { get; set; }
+
+        public string ImageId { get; set; }
+        public string ProvidedBy { get; set; }
+    }
+}
