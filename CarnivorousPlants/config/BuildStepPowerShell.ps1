e4fcda12a7d73825e09a5ea625f8d9ddefca4e65
bpelikan <bartoszpelikan@gmail.com>
2019-06-01 22:55:00 +0200

    add connection

--- CarnivorousPlants/config/BuildStepPowerShell.ps1
+++ CarnivorousPlants/config/BuildStepPowerShell.ps1
@@ -1,13 +1,32 @@
 [CmdletBinding()]
 param (
+	# $TenantId,
+	# $ApplicationId,
+	# $CertificateThumbprint,
     $ApiManagementServiceName,
     $ApiManagementServiceResourceGroup
 )
 
+# try
+# {
+
+    # "Logging in to Azure..."
+    # Add-AzureRmAccount `
+        # -ServicePrincipal `
+        # -TenantId $servicePrincipalConnection.TenantId `
+        # -ApplicationId $servicePrincipalConnection.ApplicationId `
+        # -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+# }
+# catch {
+    # Write-Error -Message $_.Exception
+	# throw $_.Exception
+# }
+
+
 $apimServiceName = $ApiManagementServiceName
 $resourceGroupName = $ApiManagementServiceResourceGroup
 
-Get-AzureRmResourceGroup | ft
+# Get-AzureRmResourceGroup | ft
 
 Write-Host "Api Management Service Name: $($apimServiceName)"
 Write-Host "Api Management Resource Group Name: $($resourceGroupName)"
