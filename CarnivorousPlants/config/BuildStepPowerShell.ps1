f73b26e244d7e1f98eda5d0df453e5dbc4d6bf0d
bpelikan <bartoszpelikan@gmail.com>
2019-06-01 22:46:28 +0200

    list all resource groups

--- CarnivorousPlants/config/BuildStepPowerShell.ps1
+++ CarnivorousPlants/config/BuildStepPowerShell.ps1
@@ -7,5 +7,7 @@ param (
 $apimServiceName = $ApiManagementServiceName
 $resourceGroupName = $ApiManagementServiceResourceGroup
 
+Get-AzureRmResourceGroup | ft
+
 Write-Host "Api Management Service Name: $($apimServiceName)"
 Write-Host "Api Management Resource Group Name: $($resourceGroupName)"
