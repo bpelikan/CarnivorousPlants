bdc8cd5377012748da0864688873e3eabd6f76cf
bpelikan <bartoszpelikan@gmail.com>
2019-06-04 00:15:09 +0200

    Create slot in web app service script

---
+++ CarnivorousPlants/CarnivorousPlants/Scripts/CreateDevelopSlot.ps1
@@ -0,0 +1,22 @@
+﻿[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]
+    [String]
+    $ResourceGroupName,
+    [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]
+    [String]
+    $ServicePlanName,
+	[Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]
+    [String]
+    $WebAppServiceName,
+    [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]
+    [String] 
+    $SlotName
+)
+
+"Create Slot:"
+"Resource group name: " + $ResourceGroupName
+"App Service Plan name: " + $ServicePlanName
+"Webb App Service Plan name: " + $WebAppServiceName
+"Slot name: "  + $SlotName
+New-AzureRmWebAppSlot -ResourceGroupName $ResourceGroupName -Name $WebAppServiceName -AppServicePlan $ServicePlanName -Slot $SlotName
