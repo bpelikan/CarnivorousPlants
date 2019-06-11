ea1cc03f0d2811d54cb30ace018c83d8179463a9
bpelikan <bartoszpelikan@gmail.com>
2019-06-11 21:16:59 +0200

    script params test

--- CarnivorousPlants/CarnivorousPlants/Scripts/ScaleDatabasePricingTier.ps1
+++ CarnivorousPlants/CarnivorousPlants/Scripts/ScaleDatabasePricingTier.ps1
@@ -24,4 +24,6 @@ param (
     $DBMaxSize
 )
 
+"ServerName" + $ServerName
+"SQLDBName" + $SQLDBName
 az sql db update -g $ResourceGroupName -s $ServerName -n $SQLDBName --tier $DBTier --capacity $DBCapacity --max-size $DBMaxSize > $null
