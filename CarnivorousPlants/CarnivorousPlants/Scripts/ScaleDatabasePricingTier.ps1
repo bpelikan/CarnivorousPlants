694288667fae8085adf56cebfdc52256a8e93fd5
bpelikan <bartoszpelikan@gmail.com>
2019-06-11 20:51:04 +0200

    script update

--- CarnivorousPlants/CarnivorousPlants/Scripts/ScaleDatabasePricingTier.ps1
+++ CarnivorousPlants/CarnivorousPlants/Scripts/ScaleDatabasePricingTier.ps1
@@ -16,10 +16,10 @@ param (
     [String] 
     $DBCapacity,
 	[Parameter(Mandatory=$true)][ValidateSet("100MB","500MB",
-											 "1GB","2GB","5GB",
-											 "10GB","20GB","30GB","40GB","50GB",
-											 "100GB","150GB","200GB","250GB","300GB","400GB","500GB","750GB"
-											 ,"1TB")] 
+			"1GB","2GB","5GB",
+			"10GB","20GB","30GB","40GB","50GB",
+			"100GB","150GB","200GB","250GB","300GB","400GB","500GB","750GB"
+			,"1TB")] 
     [String] 
     $DBMaxSize
 )
